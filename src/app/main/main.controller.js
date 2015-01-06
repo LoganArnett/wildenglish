ef4d8cc50e04f0a86d4117ccbee0648a1d580986
LoganArnett <Loganarn5@gmail.com>
2015-01-06 15:00:40 -0500

    updating navbar drop and meet the band

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,35 +3,9 @@
 angular.module('wildenglish')
   .controller('MainCtrl', function () {
     var self = this;
-    self.sounds = [
-    {track: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163232447&amp;color=1994aa&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false',
-    description: 'Track 00', url: "https://soundcloud.com/wildenglish/turn-it-on-mast"},
-    {track: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163232391&amp;color=1994aa&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
-    description: 'Track 01', url: "https://soundcloud.com/wildenglish/no-way-mast"},
-    {track: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163232342&amp;color=1994aa&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
-    description: 'Track 02', url: "https://soundcloud.com/wildenglish/giving-up"},
-    {track: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163232237&amp;color=1994aa&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
-    description: 'Track 03', url: "https://soundcloud.com/wildenglish/keep-running-mast-81414"},
-    ];
-    console.log(this.sounds[0].track)
-    this.direction = 'left';
-    this.currentIndex = 0;
-
-
-    this.next = function() {
-      this.currentIndex < this.sounds.length - 1 ? this.currentIndex++ : this.currentIndex = 0;
-    };
-
-    this.prev = function() {
-      this.currentIndex > 0 ? this.currentIndex-- : this.currentIndex = this.sounds.length - 1;
-    };
-    this.setCurrentSoundIndex = function (index) {
-      this.direction = (index > this.currentIndex) ? 'left' : 'right';
-      this.currentIndex = index;
-    };
-
-    this.isCurrentSoundIndex = function (index) {
-      return this.currentIndex === index;
-    };
-
+    
+    // function($http, $templateCache) {
+    //   this.method = 'GET';
+    //   this.url = 'https://www.googleapis.com/calendar/v3/calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com/events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU';
+    // };
   });
