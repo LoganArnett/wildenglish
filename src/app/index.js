077211f643b16b74af4a49e78cf70ca2cf36d5a2
LoganArnett <Loganarn5@gmail.com>
2015-01-12 11:54:03 -0500

    working with google cal

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,9 @@
 
 angular.module('wildenglish', ['ngAnimate', 'ngTouch', 'restangular', 'ngRoute', 'ui.bootstrap'])
   .config(function ($routeProvider, RestangularProvider) {
-    RestangularProvider.setBaseUrl('https://www.googleapis.com/calendar/v3/calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com/events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU');
-    
+
+    RestangularProvider.setBaseUrl('https://www.googleapis.com/calendar/v3');
+
     $routeProvider
       .when('/', {
         templateUrl: 'app/main/main.html',
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,9 +3,11 @@
 angular.module('wildenglish')
   .controller('MainCtrl', function (Restangular) {
     var self = this;
-    Restangular.all("items").getList();
-    //
-     console.log(Restangular.one().getList())
+    self.events =Restangular.one("calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com").customGET("events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU");
+    console.log(self.events.$object)
+    // var test = Restangular.one("calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com/events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU&callback=JSONp").getList();
+    // console.log(test)
+    //  console.log(Restangular.one("https://www.googleapis.com/calendar/v3/calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com/events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU").getList())
     // function($http, $templateCache) {
     //   this.method = 'GET';
     //   this.url = 'https://www.googleapis.com/calendar/v3/calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com/events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU';
