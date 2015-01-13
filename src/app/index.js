5c0e80f79f8e39a39b15d48e128f2fa11c658af3
LoganArnett <Loganarn5@gmail.com>
2015-01-13 12:46:25 -0500

    promise resolved

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,17 @@
 angular.module('wildenglish', ['ngAnimate', 'ngTouch', 'restangular', 'ngRoute', 'ui.bootstrap'])
   .config(function ($routeProvider, RestangularProvider) {
 
-    RestangularProvider.setBaseUrl('https://www.googleapis.com/calendar/v3');
+    RestangularProvider.setBaseUrl('https://www.googleapis.com/calendar/v3/calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com');
 
     $routeProvider
       .when('/', {
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+                googleCalEvents: function(Restangular){
+                    return Restangular.one('events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU&callback=JSON').get();
+                }
+            }
       })
       .otherwise({
         redirectTo: '/'
@@ -17,3 +22,9 @@ angular.module('wildenglish', ['ngAnimate', 'ngTouch', 'restangular', 'ngRoute',
 
   })
 ;
+/*resolve: {
+                resolvedData: function(Restangular){
+                    return Restangular.one('Items').get();
+                }
+            }
+            */
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('wildenglish')
-  .controller('MainCtrl', function (Restangular) {
+  .controller('MainCtrl', function (Restangular, googleCalEvents) {
     var self = this;
-    self.events =Restangular.one("calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com").customGET("events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU");
-    console.log(self.events.$object)
+    self.events = googleCalEvents; 
+    console.log(self.events)
     // var test = Restangular.one("calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com/events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU&callback=JSONp").getList();
     // console.log(test)
     //  console.log(Restangular.one("https://www.googleapis.com/calendar/v3/calendars/5hdm5prrvpkfl6h749ptci39eg@group.calendar.google.com/events?key=AIzaSyAZgcdMkQSiU18Hx8sgSQCKZXxljAwMDGU").getList())
