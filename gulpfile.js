553825ca771a626a67bcdedad0bf1cfece2faf23
LoganArnett <Loganarn5@gmail.com>
2015-02-09 12:07:24 -0500

    gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+});
 
 require('require-dir')('./gulp');
 
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
+
+
+
+ 
