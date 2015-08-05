64fe728a2a2ef827dd6a82b7c922d1d9fabb2e70
Logan Arnett <loganarn5@gmail.com>
2015-08-04 20:45:52 -0400

    Create loading.js

---
+++ app/loading.js
@@ -0,0 +1,263 @@
+function load_sketch_1animation(){
+  var path = document.querySelector('.path-1');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_1animation();
+
+function load_sketch_2animation(){
+  var path = document.querySelector('.path-2');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_2animation();
+
+function load_sketch_3animation(){
+  var path = document.querySelector('.path-3');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_3animation();
+
+function load_sketch_4animation(){
+  var path = document.querySelector('.path-4');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_4animation();
+
+function load_sketch_5animation(){
+  var path = document.querySelector('.path-5');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_5animation();
+
+function load_sketch_6animation(){
+  var path = document.querySelector('.path-6');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_6animation();
+
+function load_sketch_7animation(){
+  var path = document.querySelector('.path-7');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_7animation();
+
+function load_sketch_8animation(){
+  var path = document.querySelector('.path-8');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_8animation();
+
+function load_sketch_9animation(){
+  var path = document.querySelector('.path-9');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_9animation();
+
+function load_sketch_10animation(){
+  var path = document.querySelector('.path-10');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_10animation();
+
+function load_sketch_11animation(){
+  var path = document.querySelector('.path-11');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_11animation();
+
+function load_sketch_12animation(){
+  var path = document.querySelector('.path-12');
+  if(path){
+      var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 4s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}};
+
+load_sketch_12animation();
